f4a77e9db87dd55296f2dff44f594f0c187196d5
mohitkhemwani7 <47652979+mohitkhemwani7@users.noreply.github.com>
2022-09-02 19:59:10 +0530

    initial commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import './login.css'
+import {auth} from '../../firebase'
+import {signInWithEmailAndPassword} from 'firebase/auth'
+import {useNavigate, Navigate} from 'react-router-dom'
+import {useState} from "react";
+import {Nav} from "react-bootstrap";
+
+
+
+function Login() {
+    const  [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const error = useState(null);
+    const navigate = useNavigate();
+    const submitted = useState(true);
+
+
+
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        //const history = createBrowserHistory();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                //history.push('/main');
+                navigate('/main');
+            })
+            .catch((error) => {
+                this.setState({error: error});
+            });
+    }
+
+
+        return(
+            <>
+                <div className="wrapper fadeInDown">
+                    <div id="formContent">
+                        <h2 className="active"> Sign In </h2>
+                        {/*{error ? (*/}
+                        {/*    <Flex>*/}
+                        {/*        <Box>*/}
+                        {/*            <Text>{error.message}</Text>*/}
+                        {/*        </Box>*/}
+                        {/*    </Flex>*/}
+                        {/*) : null}*/}
+                        <form name="form" onSubmit={handleLogin}>
+                            <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
+                                <label htmlFor="email">Email</label><br/>
+                                <input type="text" className="form-control fadeIn second" name="email"
+                                       value={email} id="login"
+                                       onChange={e=>setEmail(e.target.value)}
+                                       />
+                                {/*{submitted && !email &&*/}
+                                {/*    <div className="help-block">Email is required</div>*/}
+                                {/*}*/}
+                            </div>
+                            <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                                <label htmlFor="password">Password</label>
+                                <input type="password" className="form-control fadeIn second" name="password"
+                                       value={password} id="password"
+                                       onChange={e=>setPassword(e.target.value)} />
+                                {/*{submitted && !password &&*/}
+                                {/*    <div className="help-block">Password is required</div>*/}
+                                {/*}*/}
+                                <input type="submit" className="fadeIn fourth" value="Log In"/>
+                            </div>
+
+                            {/*<Container>*/}
+                            {/*    <Row>*/}
+                            {/*        <h5>Not a User?</h5>*/}
+                            {/*        <h5 > Register</h5>*/}
+                            {/*    </Row>*/}
+                            {/*</Container>*/}
+
+                        </form>
+                    </div>
+                </div>
+            </>
+        )
+}
+
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBr2V9cdipYee6KNu1h6LE-DMJdoD6YmK8",
+    authDomain: "whistlebugle-2d1d0.firebaseapp.com",
+    databaseURL: "https://whistlebugle-2d1d0-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "whistlebugle-2d1d0",
+    storageBucket: "whistlebugle-2d1d0.appspot.com",
+    messagingSenderId: "499421700362",
+    appId: "1:499421700362:web:e6e0f1cd34085d7e111af3",
+    measurementId: "G-PZ5H6M3XF4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
