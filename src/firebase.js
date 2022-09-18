29e05cc62dec1dea9a584f1c5adb7b39279b8d79
mohitkhemwani7 <47652979+mohitkhemwani7@users.noreply.github.com>
2022-09-19 00:48:33 +0530

    Firebase Envrionment Config

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBr2V9cdipYee6KNu1h6LE-DMJdoD6YmK8",
-    authDomain: "whistlebugle-2d1d0.firebaseapp.com",
-    databaseURL: "https://whistlebugle-2d1d0-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "whistlebugle-2d1d0",
-    storageBucket: "whistlebugle-2d1d0.appspot.com",
-    messagingSenderId: "499421700362",
-    appId: "1:499421700362:web:e6e0f1cd34085d7e111af3",
-    measurementId: "G-PZ5H6M3XF4"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
